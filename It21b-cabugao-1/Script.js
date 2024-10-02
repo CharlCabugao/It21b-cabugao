5a6d1595a9bb67171aaffa1c7a2c4e788221759d
CharlCabugao <charlcabugao29@gmail.com>
2024-10-02 05:25:10 -0700

    Add files via upload

---
+++ It21b-cabugao-1/Script.js
@@ -0,0 +1,44 @@
+class Person {
+    #name; 
+    #age;  
+    #occupation; 
+
+    constructor(name, age, occupation) {
+        this.#name = name;
+        this.#age = age;
+        this.#occupation = occupation;
+    }
+
+    getName() {
+        return this.#name;
+    }
+
+    getAge() {
+        return this.#age;
+    }
+
+    getOccupation() {
+        return this.#occupation;
+    }
+}
+
+class Student extends Person {
+    constructor(name, age, occupation, major) {
+        super(name, age, occupation);
+        this.major = major;
+    }
+
+    getMajor() {
+        return this.major;
+    }
+}
+
+const student = new Student('charl', 19, 'Student', 'Programming');
+
+const studentInfoDiv = document.getElementById('student-info');
+studentInfoDiv.innerHTML = `
+    <p>Name: ${student.getName()}</p>
+    <p>Age: ${student.getAge()}</p>
+    <p>Occupation: ${student.getOccupation()}</p>
+    <p>Major: ${student.getMajor()}</p>
+`;
